2fa515926f93b892ffba38fe67d3392748747646
Richard Beaver <beaver.r@northeastern.edu>
2024-03-20 21:33:14 -0400

    Adding other ways to bind `this`

--- js120-object-oriented-prog/function-context/func-context-02.js
+++ js120-object-oriented-prog/function-context/func-context-02.js
@@ -2,19 +2,19 @@
 
 // The method franchise.allMovies is supposed to return the following array:
 [
-  'How to Train Your Dragon 1',
-  'How to Train Your Dragon 2',
-  'How to Train Your Dragon 3'
+  "How to Train Your Dragon 1",
+  "How to Train Your Dragon 2",
+  "How to Train Your Dragon 3",
 ];
 
 // Explain why this method will not return the desired object? Try fixing this
 // problem by taking advantage of JavaScript lexical scoping rules.
 
 let franchise = {
-  name: 'How to Train Your Dragon',
-  allMovies: function() {
-    return [1, 2, 3].map(function(number) {
-      return this.name + ' ' + number;
+  name: "How to Train Your Dragon",
+  allMovies: function () {
+    return [1, 2, 3].map(function (number) {
+      return this.name + " " + number;
     });
   },
 };
@@ -40,14 +40,39 @@ This ensures that the callback's execution context in each iteration is the
 
 */
 
-let franchise = {
-  name: 'How to Train Your Dragon',
-  allMovies: function() {
-    let self = this;
+franchise = {
+  name: "How to Train Your Dragon",
+  allMovies: function () {
+    // 1.
 
-    return [1, 2, 3].map(function(number) {
-      return self.name + ' ' + number;
-    });
+    // Using a `self` variable outside of the callback definition
+    // let self = this;
+    // return [1, 2, 3].map(function (number) {
+    //   return self.name + " " + number;
+    // });
+
+    // 2.
+
+    // Binding `map` to the calling object by passing `this` as an optional
+    // `thisArg` argument of the `map` method
+    // return [1, 2, 3].map(function (number) {
+    //   return this.name + " " + number;
+    // }, this);
+
+    // 3.
+
+    // Hard-binding `this` to the function expression passed to `map`
+    // return [1, 2, 3].map(
+    //   function (number) {
+    //     return this.name + " " + number;
+    //   }.bind(this)
+    // );
+
+    // 4.
+
+    // Using an arrow function to ensure `this` is kept bound to the current
+    // execution context
+    return [1, 2, 3].map((number) => this.name + " " + number);
   },
 };
 
